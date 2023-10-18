fead1ab68c96334a04417dbba6142da77939c695
XiangGao <392041299@qq.com>
2023-10-18 22:24:25 +0800

    chore: 格式化文件

--- playground/app.vue
+++ playground/app.vue
@@ -2,8 +2,4 @@
   <div>Nuxt module playground!</div>
 </template>
 
-<script lang="ts" setup>
-import { useHttp } from '#imports'
-
-console.log(useHttp().get('/user'))
-</script>
+<script lang="ts" setup></script>
